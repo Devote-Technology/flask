0896cd76fef235462e5b3ff2be23ffc808ca9c08
Jack Wheatley <78288520+jackdw22@users.noreply.github.com>
2023-02-21 19:51:10 -0700

    deleting hardcode

--- db.py
+++ db.py
@@ -9,8 +9,6 @@ dbPassword: str = os.environ.get("DB_PASSWORD")
 host: str = os.environ.get("DB_HOST")
 port: str = os.environ.get("DB_PORT")
 
-dbPassword = "1jL4urQKr2i4oHr8"
-
 
 def getConnection():
   #conn = psycopg2.connect(dbname="postgres", user="postgres", password=dbPassword,host="db.zkpmfmkldxumszdjxbse.supabase.co", port=5432)
